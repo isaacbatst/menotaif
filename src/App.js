8b7dd917a1f8543a951ccc49787bf7466196dcfa
Isaac Batista <isaacbatst@gmail.com>
2020-01-02 22:34:31 -0300

    metadata

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Button } from 'antd';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Calcule suas notas na Web
         </p>
