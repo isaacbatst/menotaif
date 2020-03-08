69803f49a540c2076235890e4c754c4abda5fe85
Isaac Batista <isaacbatst@gmail.com>
2020-03-08 15:39:31 -0300

    feat: use grades validator

--- src/components/SecondStepContent/index.js
+++ src/components/SecondStepContent/index.js
@@ -8,10 +8,17 @@ import GradesValidator from "../../classes/gradesValidator";
 
 export default ({ dispatchNextStep }) => {
   const dispatch = useDispatch();
-  const [buttonDisabled, setButtonDisabled] = useState(true)
+  const [buttonDisabled, setButtonDisabled] = useState(true);
   const gradesObject = useSelector(state => state.steps.subjectType.grades);
   const selectedGradeType = useSelector(state => state.steps.subjectType.type);
+  const [gradesValidator, setGradesValidator] = useState(
+    GradesValidator({ selectedGradeType })
+  );
   const grades = Object.entries(gradesObject);
+
+  useEffect(() => {
+    setGradesValidator(GradesValidator({ selectedGradeType }));
+  }, [selectedGradeType]);
 
   const handleBackButton = () => {
     dispatchNextStep({ currentStep: 0, subjectType: null });
@@ -22,14 +29,12 @@ export default ({ dispatchNextStep }) => {
   };
 
   useEffect(() => {
-    const gradesValidator = GradesValidator({ selectedGradeType });
-    
-    if(gradesValidator.validate(gradesObject)){
+    if (gradesValidator.validate(gradesObject)) {
       setButtonDisabled(false);
     } else {
-      setButtonDisabled(true)
+      setButtonDisabled(true);
     }
-  }, [grades, gradesObject, selectedGradeType]);
+  }, [grades, gradesValidator, gradesObject, selectedGradeType]);
 
   return (
     <>
@@ -48,7 +53,13 @@ export default ({ dispatchNextStep }) => {
       </Row>
       <Row style={{ display: "flex", justifyContent: "center" }}>
         <Col xs={12} md={6} className="text-align-center">
-          <Button size="large" disabled={buttonDisabled} id="calc-button" block type="primary">
+          <Button
+            size="large"
+            disabled={buttonDisabled}
+            id="calc-button"
+            block
+            type="primary"
+          >
             Calcular
           </Button>
           <Button type="danger" block onClick={handleBackButton}>
