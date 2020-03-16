b2ebca1b98a4d6650841ec6ad7ada4b64a257406
Isaac Batista <isaacbatst@gmail.com>
2020-03-16 20:21:41 -0300

    test: create grades validator test

---
+++ src/classes/gradesValidator.test.js
@@ -0,0 +1,35 @@
+import SUBJECT_TYPES from "../constants/subjectTypes";
+import createGradesValidator from "./gradesValidator";
+
+describe("tests grades validator for annual grades", () => {
+  const gradesValidator = createGradesValidator({
+    selectedGradeType: SUBJECT_TYPES.annual.type
+  });
+
+  it("should return false when first grade is undefined", () => {
+    const gradesWithFirstGradeMissing = {
+      secondGrade: { value: 50 },
+      thirdGrade: { value: 40 }
+    };
+    expect(gradesValidator.validate(gradesWithFirstGradeMissing)).toBe(false);
+  });
+
+  it("should return false when second grade is undefined", () => {
+    const gradesWithSecondGradeMissing = {
+      firstGrade: { value: 10 },
+      thirdGrade: { value: 20 }
+    };
+    expect(gradesValidator.validate(gradesWithSecondGradeMissing)).toBe(false);
+  });
+
+  it("should return true when first and second grades are defined", () => {
+    const gradesWithFirstAndSecondGradesDefined = {
+      firstGrade: { value: 10 },
+      secondGrade: { value: 20 },
+      thirdGrade: { value: 30 }
+    };
+    expect(
+      gradesValidator.validate(gradesWithFirstAndSecondGradesDefined)
+    ).toBe(true);
+  });
+});
