ccb6abc3428e689c9ed7f49e59752ff8e5489bf7
Isaac Batista <isaacbatista@rits.com.br>
2020-03-26 00:11:24 -0300

    refactor: extract social sharing constants to a separate file

--- src/components/Content/Footer.js
+++ src/components/Content/Footer.js
@@ -1,24 +1,22 @@
 import React from 'react';
+import SOCIAL_SHARING from '../../constants/socialSharing';
 import { WhatsappShareButton, TwitterShareButton, TelegramShareButton, FacebookShareButton, WhatsappIcon, TwitterIcon, TelegramIcon, FacebookIcon } from 'react-share'
 
 export default () => {
-  const url = "https://menotaif.codandomuito.com.br";
-  const message = "Saca só que massa, dá pra a gente ver quanto precisa tirar pra passar!"
-
   return (
     <div>
       <p>Compartilhe com a galera!</p>
       <div id="social-row">
-        <WhatsappShareButton url={url} title={message} >
+        <WhatsappShareButton url={SOCIAL_SHARING.url} title={SOCIAL_SHARING.message} >
           <WhatsappIcon />
         </WhatsappShareButton>
-        <TwitterShareButton url={url} title={message}>
+        <TwitterShareButton url={SOCIAL_SHARING.url} title={SOCIAL_SHARING.message}>
           <TwitterIcon />
         </TwitterShareButton>
-        <TelegramShareButton url={url} title={message}>
+        <TelegramShareButton url={SOCIAL_SHARING.url} title={SOCIAL_SHARING.message}>
           <TelegramIcon />
         </TelegramShareButton>
-        <FacebookShareButton url={url} title={message}>
+        <FacebookShareButton url={SOCIAL_SHARING.url} title={SOCIAL_SHARING.message}>
           <FacebookIcon />
         </FacebookShareButton>
       </div>
---
+++ src/constants/socialSharing.js
@@ -0,0 +1,4 @@
+export default {
+  url: "https://menotaif.codandomuito.com.br",
+  message: "Saca só que massa, dá pra a gente ver quanto precisa tirar pra passar!"
+}
